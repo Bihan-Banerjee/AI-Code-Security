c8b9fbe4076785e17cf0e78e538c7b186edb42e3
Nethra Krishnan <162780226+nethra0906@users.noreply.github.com>
2025-06-29 22:03:16 +0530

    Create user.java

---
+++ ChatGPT/JS/Code/user.java
@@ -0,0 +1,20 @@
+package com.example.auth.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+    private String bio;
+    private String phone;
+    private String profileImage;  // File path
+}
