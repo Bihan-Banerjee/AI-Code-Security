9b4ffdf5ffb27634b82675808a7240207bfa5cf5
Nethra Krishnan <162780226+nethra0906@users.noreply.github.com>
2025-06-30 22:11:24 +0530

    Create register.java

---
+++ DeepSeek/Java/Code/register.java
@@ -0,0 +1,32 @@
+package com.example.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    
+    @NotBlank(message = "Name is required")
+    private String name;
+    
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
+    private String email;
+    
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    @NotBlank(message = "Password is required")
+    private String password;
+    
+    private String bio;
+    private String phone;
+    private MultipartFile profileImage;
+}
