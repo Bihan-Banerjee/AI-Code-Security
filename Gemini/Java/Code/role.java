790ae4d72a8cea558e304dfb697b4f9d1c451828
Nethra Krishnan <162780226+nethra0906@users.noreply.github.com>
2025-07-09 22:01:46 +0530

    Create role.java

---
+++ Gemini/Java/Code/role.java
@@ -0,0 +1,26 @@
+package com.example.authapp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private RoleName name;
+
+    public enum RoleName {
+        ROLE_USER,
+        ROLE_ADMIN
+    }
+}
